8adff32dea50b3966705a581c23cf56269358fe3
deonolarewaju <deonolarewaju@gmail.com>
2022-08-15 23:18:48 +0100

    into the void

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+const damo = 'damo'
+const seun = 'seun'
+const femi = 'semi'
+
+const number = '10'
+
+const sayHi = (name) => {
+    console.log(`please say hi to ${name}`)
+}
+
+const sayNumber = (number) => {
+
+    parseInt(number)
+
+    return console.log(`please say hi to ${number}`)
+    
+}
+
+
+console.log(sayHi(damo))
+console.log(sayHi(seun))
+console.log(parseInt(number))
