95fd6e5cae5a1a2b1440fee888036c95514c0761
deonolarewaju <deonolarewaju@gmail.com>
2023-08-24 01:13:03 +0100

    Working on some fixes on undefined data

--- app.js
+++ app.js
@@ -2,11 +2,8 @@
 
 const names = require('./2-names')
 const functions = require('./3-utils')
-const data = require('./4-alternative_export_modules')
-//const sayHi = require('./5-sayHi')
 
-console.log(names, functions, data)
+console.log(names, functions)
 
 
 functions.sayHi(names.femi)
-//console.log(module)
